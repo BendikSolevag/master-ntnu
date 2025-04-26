c83acf22be51e2d55a1b76d977211ef18d3d08a8
Bendik Solevåg <bendik.solevag@vacc.no>
2025-04-26 08:57:59 +0200

    Showcase optimal timing works with tdlambda, custom growth function

--- test.py
+++ test.py
@@ -14,11 +14,14 @@ Everything else (network, environment, plotting) is identical.
 import time
 import collections
 from matplotlib import pyplot as plt
+import numpy as np
 import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm
+
+from main.environment import GrowthNN
 
 
 class ActorCriticNetwork(nn.Module):
@@ -39,7 +42,7 @@ class ActorCriticNetwork(nn.Module):
         return self.pi(x), self.v(x)
 
 class Agent:
-    def __init__(self, lr, input_dims, n_actions, fc1_dims=256, fc2_dims=256, gamma=0.99, n_step=5):
+    def __init__(self, lr, input_dims, n_actions, fc1_dims=256, fc2_dims=256, gamma=0.99, n_step=200):
         self.gamma = gamma
         self.n_step = n_step
         self.net = ActorCriticNetwork(lr, input_dims, n_actions, fc1_dims, fc2_dims)
@@ -108,37 +111,55 @@ class Agent:
 
 class TEnv:
     def __init__(self):
-        self.state = 0
+        self.state = 0.2
         self.DONE  = False
+        self.AGE = 0
+        # Growth rate NN
+        self.growth_model = GrowthNN(input_size=4)
+        self.growth_model.load_state_dict(T.load('./models/growth/1743671011.288821-model.pt', weights_only=True))
+        self.growth_model.eval()
+
+    def resolve_growth_open(self):
+      explanatory = [
+          round(self.AGE), #generation_approx_age, 
+          self.state * 0.015 * 30, #feedamountperfish, 
+          self.state, #mean_size,
+          0.25, #mean_voksne_hunnlus,
+      ]
+      pred = self.growth_model.forward(T.tensor(explanatory, dtype=T.float32)).item()  
+      # Cap prediction within reasonable range
+      pred = max(min(pred, 8), 0.1)
+      # Adjust monthly to weekly
+      g_rate = np.log(pred / self.state) / 4.345
+      self.state *= np.exp(g_rate)
 
     def get_state(self):
         return [self.state]
 
     def step(self, action: int):
-        reward = -1
+        reward = -0.01
+        self.AGE += 1/52
         if action == 1:
-            reward = 20 - (4 - self.state) ** 2
+            reward += self.state
+            reward -= 7
             self.DONE = True
-        self.state += 1
+        self.resolve_growth_open()
         return reward, self.DONE
 
-def visualize_env():
 
-    trews = []
-    for i in range(120):
-        env = TEnv()
-        trew = 0
-        for j in range(i):
-            rew, done = env.step(0)
-            trew += rew
-        rew, done = env.step(1)
-        trew += rew
-        trews.append(trew)
-    
-    plt.plot(trews)
-    plt.show()
-                      
-            
+def visualize_env():
+  trews = []
+  for i in range(120):
+    env = TEnv()
+    trew = 0
+    for j in range(i):
+      rew, done = env.step(0)
+      trew += rew
+    rew, done = env.step(1)
+    trew += rew
+    trews.append(trew)
+  return trews
+
 
 def main():
   env   = TEnv()
@@ -149,11 +170,11 @@ def main():
                 n_actions=2,
                 fc1_dims=4, fc2_dims=4,
                 gamma=0.99,
-                n_step=5)
+                n_step=200)
 
   episode_lengths = []
 
-  for ep in tqdm(range(1000)):
+  for ep in tqdm(range(1500)):
     env = TEnv()
     state = env.get_state()
     timesteps = 0
@@ -162,7 +183,7 @@ def main():
       action = agent.choose_action(state)
 
       # safety stop: force terminate after 20 steps
-      if timesteps > 20:
+      if timesteps > 200:
         action = 1
 
       reward, done = env.step(action)
@@ -177,14 +198,18 @@ def main():
       timesteps += 1
 
     episode_lengths.append(timesteps)
+  return episode_lengths
 
-  # plot how long the agent waits before terminating
-  plt.plot(episode_lengths)
-  plt.xlabel("Episode")
-  plt.ylabel("Steps before terminating")
-  plt.show()
+
 
 
 if __name__ == "__main__":
-  #main()
-  visualize_env()
+
+  fig, ax = plt.subplots(2, 1)
+  
+  trews = visualize_env()
+  lens = main()
+
+  ax[0].plot(trews)
+  ax[1].plot(lens)
+  plt.show()
